061519c3682973b38dd7e21c5b0a32c51701ced3
zkxjzmswkwl <smithcarterfreelance@gmail.com>
2025-01-20 04:44:30 -0700

    Update function addresses for 940-1.

--- source/rdconstants.d
+++ source/rdconstants.d
@@ -21,28 +21,36 @@ __gshared MAX_ADDRESS = 0x7FFFFFFF0000uL;
 __gshared MIN_ADDRESS = 0x100000uL;
 
 const ulong CLIENT_PTR           = 0xD89758;
-const ulong MOUSE_HOOK           = 0xD7E078;
-// Wouldn't rely on this sig.
+const ulong MOUSE_HOOK           = 0xD7F078;
 // 48 89 5C 24 ? 48 89 74 24 ? 48 89 7C 24 ? 55 41 56 41 57 48 8D 6C 24 ? 48 81 EC ? ? ? ? 4C 63 79
-const ulong FN_ADD_CHAT          = 0xCE8A0;
+// XREF Signature #1 @ EB2F9: E8 ? ? ? ? 48 8B D0 48 8B CB E8 ? ? ? ? E9
+// XREF Signature #2 @ E7AE6: E8 ? ? ? ? 48 8B D0 48 8B CB E8 ? ? ? ? 0F B6 85
+// XREF Signature #3 @ 97EB1: E8 ? ? ? ? 48 8B D0 48 8B CE E8 ? ? ? ? 0F B6 44 24
+// XREF Signature #4 @ E9F5F: E8 ? ? ? ? 48 8B D0 48 8B CF E8 ? ? ? ? 0F B6 84 24
+// XREF Signature #5 @ E8F53: E8 ? ? ? ? 48 8B D0 48 8B CB E8 ? ? ? ? 0F B6 45 ? 48 8D 1D
+const ulong FN_ADD_CHAT          = 0xCE880;
 // 48 89 5C 24 ? 0F B6 41 ? 4C 8B C9
 const ulong FN_UPDATE_STAT       = 0x272980;
 // 48 89 5C 24 ? 48 89 74 24 ? 57 48 83 EC ? 48 83 79 ? ? 41 0F B6 F0 8B FA
-const ulong FN_HIGHLIGHT_ENTITY  = 0x355300;
+// const ulong FN_HIGHLIGHT_ENTITY  = 0x355300;
+const ulong FN_HIGHLIGHT_ENTITY  = 0x355710;
 // 40 57 48 83 EC ? 48 8B 79 ? 4C 8B D9
-const ulong FN_HIGHLIGHT         = 0x1245F0;
+// const ulong FN_HIGHLIGHT         = 0x1245F0;
+const ulong FN_HIGHLIGHT         = 0x1247F0;
 // 40 53 48 81 EC ? ? ? ? 48 8B 41 ? 45 8B D9 44 8B 94 24
-const ulong FN_RENDER_MENU_ENTRY = 0x418730;
+const ulong FN_RENDER_MENU_ENTRY = 0x418B72;
 // 48 8B 99 ? ? ? ? 48 3B 99 20 98 01 00 74 24
 const ulong FN_SET_CLIENT_STATE  = 0x25A00;
 // 44 89 44 24 ? 53 41 55
+// const ulong FN_RUN_SCRIPT        = 0x8B9E0;
 const ulong FN_RUN_SCRIPT        = 0x8B9E0;
 // 40 55 41 54 41 55 41 56 41 57 48 8D AC 24
-const ulong FN_ADD_ENTRY_INNER   = 0x14EDA0;
+// const ulong FN_ADD_ENTRY_INNER   = 0x14EDA0;
+const ulong FN_ADD_ENTRY_INNER   = 0x14F230;
 // 48 89 5C 24 ? 4C 8B 59 ? 44 8D 14 12
-const ulong FN_GET_INVENTORY     = 0x2D7280;
+const ulong FN_GET_INVENTORY     = 0x2D76C0;
 // 44 88 44 24 ? 89 54 24 ? 56
-const ulong FN_GET_COMP_BY_ID    = 0x2DFCA0;
+const ulong FN_GET_COMP_BY_ID    = 0x2E00E0;
 
 // Client
 const ulong OF_CLIENT_STATE     = 0x19F48;
